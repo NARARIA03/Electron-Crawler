2fea26f072c76ccad8f9d87fda9bab3de7113e51
NARARIA03 <hyeonseong0305@gmail.com>
2025-05-24 18:44:01 +0900

    refactor: 콜백 함수 제거 리팩토링

--- selenium/configs/Browser.py
+++ selenium/configs/Browser.py
@@ -26,14 +26,12 @@ class Browser:
         print("웹드라이버 초기 설정 성공")
         self.driver.get(url)
         time.sleep(2)
+        self.curWindowHandle = self.driver.current_window_handle
         print("현재 페이지: ", self.driver.current_url)
 
     def close(self) -> None:
         self.driver.quit()
         print("웹드라이버 종료 완료")
-
-    def getElement(self, by: ByType, value: str) -> WebElement:
-        return self.driver.find_element(by, value)
 
     def clickElement(self, by: ByType, value: str) -> None:
         WebDriverWait(self.driver, 10).until(
@@ -47,53 +45,32 @@ class Browser:
         ).send_keys(input)
         print("입력 완료")
 
-    def actionToNewIframe(
-        self,
-        by: ByType,
-        value: str,
-        iframeBy: ByType,
-        iframeValue: str,
-        callbackFn: Callable[["Browser"], Any],
-    ) -> None:
-        self.clickElement(by, value)
+    def focusIframe(self, by: ByType, value: str) -> None:
         WebDriverWait(self.driver, 10).until(
-            EC.frame_to_be_available_and_switch_to_it((iframeBy, iframeValue))
+            EC.frame_to_be_available_and_switch_to_it((by, value))
         )
         print("iframe 전환 완료")
-        try:
-            callbackFn(self)
-        except Exception as e:
-            print("에러 발생:", e)
-        finally:
-            self.driver.switch_to.default_content()
-            print("iframe 복귀 완료")
 
-    def actionToNewWindow(
-        self,
-        by: ByType,
-        value: str,
-        callbackFn: Callable[["Browser"], Any],
-    ) -> None:
-        originalWindow = self.driver.current_window_handle
+    def unfocusIframe(self) -> None:
+        self.driver.switch_to.default_content()
+        print("기본 컨텐츠로 전환 완료")
+
+    def goToNewWindow(self, by: ByType, value: str) -> None:
         originalHandles = set(self.driver.window_handles)
 
         self.clickElement(by, value)
         WebDriverWait(self.driver, 10).until(
             lambda d: len(d.window_handles) > len(originalHandles)
         )
+
         curHandles = set(self.driver.window_handles)
         newHandle = curHandles - originalHandles
         if not newHandle:
             raise RuntimeError("새 창 전환 실패: 새로운 윈도우를 찾을 수 없습니다")
-        newWindow = newHandle.pop()
-        self.driver.switch_to.window(newWindow)
-        print(f"새 창 포커스 전환 완료: {newWindow}")
 
-        try:
-            callbackFn(self)
-        except Exception as e:
-            print("에러 발생:", e)
-        finally:
-            self.driver.close()
-            self.driver.switch_to.window(originalWindow)
-            print("원래 창 복귀 완료")
+        self.driver.switch_to.window(newHandle.pop())
+        print("새 윈도우로 전환 완료")
+
+    def goToDefaultWindow(self) -> None:
+        self.driver.switch_to.window(self.curWindowHandle)
+        print("초기 윈도우로 이동 완료")
--- selenium/main.py
+++ selenium/main.py
@@ -19,46 +19,40 @@ if __name__ == "__main__":
         browser.typingInputElement("xpath", '//*[@id="m_input"]', MOCK["query"])
         # 검색 버튼 클릭
         browser.clickElement("xpath", '//*[@id="mainBackImg"]/div[2]/div[1]/div/button')
-        # 상세 검색 클릭해서 iframe 모달 열기
-        browser.actionToNewIframe(
+        # 상세 검색 클릭
+        browser.clickElement("xpath", '//*[@id="srchBtnDiv"]/a')
+        # iframe에 focus
+        browser.focusIframe("id", "modalIfm")
+        # 초중고등학교 포함 클릭
+        browser.clickElement(
             "xpath",
-            '//*[@id="srchBtnDiv"]/a',
-            "id",
-            "modalIfm",
-            lambda b: (
-                # 초중고등학교 포함 클릭
-                b.clickElement(
-                    "xpath",
-                    '//*[@id="popup_wrap"]/div[2]/div/div[1]/div/table/tbody/tr[1]/td/p/label',
-                ),
-                # 기관찾기 클릭
-                b.actionToNewWindow(
-                    "xpath",
-                    '//*[@id="popup_wrap"]/div[2]/div/div[1]/div/table/tbody/tr[3]/td/div/button',
-                    lambda b: (
-                        # 기관명 입력
-                        b.typingInputElement(
-                            "xpath", '//*[@id="indvdlzInsttNm"]', MOCK["organization"]
-                        ),
-                        # 검색 클릭
-                        b.clickElement(
-                            "xpath",
-                            '//*[@id="popup_wrap"]/div[2]/div[1]/table/tbody/tr/td/div/button[1]',
-                        ),
-                        # 검색 결과에 organization과 location이 모두 포함된 요소 클릭
-                        b.clickElement(
-                            "xpath",
-                            f"//ul[contains(@class,'jstree-no-dots')]/li/a"
-                            f"[contains(@title,'{MOCK['location']}') and "
-                            f"contains(@title,'{MOCK['organization']}')]",
-                        ),
-                        # 확인 버튼 클릭
-                        b.clickElement(
-                            "xpath", '//*[@id="popup_wrap"]/div[2]/div[2]/div[5]/a[1]'
-                        ),
-                    ),
-                ),
-            ),
+            '//*[@id="popup_wrap"]/div[2]/div/div[1]/div/table/tbody/tr[1]/td/p/label',
         )
+        # 기관찾기 클릭
+        browser.goToNewWindow(
+            "xpath",
+            '//*[@id="popup_wrap"]/div[2]/div/div[1]/div/table/tbody/tr[3]/td/div/button',
+        )
+        # 기관명 입력
+        browser.typingInputElement(
+            "xpath", '//*[@id="indvdlzInsttNm"]', MOCK["organization"]
+        )
+        # 검색 클릭
+        browser.clickElement(
+            "xpath",
+            '//*[@id="popup_wrap"]/div[2]/div[1]/table/tbody/tr/td/div/button[1]',
+        )
+        # 검색 결과에 organization과 location이 모두 포함된 요소 클릭
+        browser.clickElement(
+            "xpath",
+            f"//ul[contains(@class,'jstree-no-dots')]/li/a"
+            f"[contains(@title,'{MOCK['location']}') and "
+            f"contains(@title,'{MOCK['organization']}')]",
+        )
+        # 확인 버튼 클릭 (새 창 자동으로 닫힘)
+        browser.clickElement("xpath", '//*[@id="popup_wrap"]/div[2]/div[2]/div[5]/a[1]')
+        browser.goToDefaultWindow()
+        time.sleep(10)
+
     except Exception as e:
         print("에러 발생:", e)
