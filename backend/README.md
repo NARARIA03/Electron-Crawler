0fd9986e4f35098ee78e119774e74eca43364ed9
NARARIA03 <hyeonseong0305@gmail.com>
2025-05-25 02:07:21 +0900

    feat: 백엔드 시작점 구현 (pyinstaller로 구현)

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+# 빌드 방법
+
+```shell
+pyinstaller --onefile --name mycrawler src/main.py
+```
+
+## MacOS
+
+```shell
+/dist/mycrawler --type open-go-kr --data '[{"query":"전자칠판","organization":"서울서일초등학교","location":"서울특별시교육청","startDate":"2025-02-19","endDate":"2025-05-22"}]'
+```
---
+++ backend/src/main.py
@@ -0,0 +1,38 @@
+import argparse
+import sys, json
+
+TYPE = ["open-go-kr", "nara-g2b-portal", "computime-alert"]
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("사용법: '<JSON 배열 또는 객체>'")
+        sys.exit(1)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--type", choices=TYPE, required=True)
+    parser.add_argument("--data", type=str, required=True)
+    args = parser.parse_args()
+
+    try:
+        configs = json.loads(args.data)
+    except json.JSONDecodeError:
+        print("--data 파라미터 이슈")
+        sys.exit(1)
+    if not isinstance(configs, list):
+        print("--data 파라미터 이슈")
+        sys.exit(1)
+
+    if args.type == "open-go-kr":
+        print(configs, args.type)
+        return
+    elif args.type == "nara-g2b-portal":
+        print(configs, args.type)
+        return
+    else:
+        print(configs, args.type)
+        return
+
+
+if __name__ == "__main__":
+    main()
