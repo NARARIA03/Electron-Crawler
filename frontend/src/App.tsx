3cc2f38db134917fa4cd353f2b60891b9aa3bda4
NARARIA03 <hyeonseong0305@gmail.com>
2025-05-31 18:03:05 +0900

    feat: shadcn 컴포넌트로 랜딩 페이지 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import { Button, H1, Paragraph } from "./components";
+import { toast } from "sonner";
+
 function App() {
-  return <></>;
+  const handleNotImplementedClick = () => toast.warning("아직 구현 중인 기능입니다.");
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-24 bg-slate-800">
+      <H1>크롤링 서비스 (Beta)</H1>
+      <div className="flex justify-center items-center gap-24">
+        <Button variant="secondary" size="lg" className="cursor-pointer">
+          <Paragraph>정보공개포털</Paragraph>
+        </Button>
+        <Button variant="secondary" size="lg" className="cursor-pointer" onClick={handleNotImplementedClick}>
+          <Paragraph>컴시간알리미</Paragraph>
+        </Button>
+        <Button variant="secondary" size="lg" className="cursor-pointer" onClick={handleNotImplementedClick}>
+          <Paragraph>나라장터</Paragraph>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/ui/paragraph.tsx
@@ -0,0 +1,27 @@
+import type { HTMLAttributes, PropsWithChildren } from "react";
+
+type HeadingProps = PropsWithChildren<HTMLAttributes<HTMLHeadingElement>>;
+
+type ParagraphProps = PropsWithChildren<HTMLAttributes<HTMLParagraphElement>>;
+
+export const H1 = (props: HeadingProps) => {
+  return (
+    <h1
+      className="scroll-m-20 text-slate-200 text-center text-4xl font-extrabold tracking-tight text-balance pointer-events-none select-none"
+      {...props}
+    />
+  );
+};
+
+export const H2 = (props: HeadingProps) => {
+  return (
+    <h2
+      className="scroll-m-20 text-slate-200 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 pointer-events-none select-none"
+      {...props}
+    />
+  );
+};
+
+export const Paragraph = (props: ParagraphProps) => {
+  return <p className="leading-7 [&:not(:first-child)]:mt-6 pointer-events-none select-none" {...props} />;
+};
