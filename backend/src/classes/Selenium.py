03129c11b178b0de15e2b003f4aa1a6fd1275868
NARARIA03 <hyeonseong0305@gmail.com>
2025-06-24 00:38:02 +0900

    fix: 윈도우 정상 작동 확인 완료, selenium headless 모드 재설정

--- backend/src/classes/Selenium.py
+++ backend/src/classes/Selenium.py
@@ -24,7 +24,7 @@ class Selenium:
         }
 
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--start-maximized")
         options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(options=options)
