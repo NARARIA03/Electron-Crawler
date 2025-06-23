2dfa54c62c1c244b8124445d0750d4622991a982
NARARIA03 <hyeonseong0305@gmail.com>
2025-06-23 22:03:44 +0900

    fix: 빌드 시 query.xlsx를 다운하지 못 하는 문제 해결

--- frontend/electron/ipcs/downloadQueryExcel.ts
+++ frontend/electron/ipcs/downloadQueryExcel.ts
@@ -1,14 +1,21 @@
 import { app, ipcMain } from "electron";
 import fs from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const downloadQueryExcel = () => {
   ipcMain.handle("download-excel", async () => {
     const downloadsDir = app.getPath("downloads");
     const dest = path.join(downloadsDir, "query.xlsx");
 
-    const basePath = app.isPackaged ? process.resourcesPath : app.getAppPath();
-    const src = path.join(basePath, "public", "excel", "query.xlsx");
+    const src = app.isPackaged
+      ? path.join(process.resourcesPath, "excel", "query.xlsx") // 배포 후
+      : path.join(__dirname, "..", "excel", "query.xlsx"); // 개발 중
+
+    console.log(src, dest);
+
     await fs.promises.copyFile(src, dest);
     return dest;
   });
