dccf52c7f3b0df07322f2e1b1cf8a76b7d449f6b
NARARIA03 <hyeonseong0305@gmail.com>
2025-08-06 02:25:34 +0900

    chore: 안쓰는 상수 제거 #14

--- frontend/src/constants/ipc.ts
+++ frontend/src/constants/ipc.ts
@@ -1,14 +1,4 @@
 export const SELECT_DIRECTORY = "select-download-dir";
-export const PROCESS_NAME = {
-  win: "script.exe",
-  mac: "script",
-};
-export const PYTHON = {
-  run: "python-run",
-  result: "python-result",
-  stdout: "python-stdout",
-  stderr: "python-stderr",
-};
 
 export const OPEN_FINDER = "open-finder";
 
