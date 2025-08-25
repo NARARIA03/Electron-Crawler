96380c04d05a6da7ea5c93aaec48e1ef5cfb8563
NARARIA03 <hyeonseong0305@gmail.com>
2025-08-26 00:37:30 +0900

    feat: 프론트단 IPC 레이어 구현

---
+++ frontend/src/features/naraG2b/types/index.ts
@@ -0,0 +1,17 @@
+export type NaraG2bTaskFE = {
+  id: string;
+  data: NaraG2bDataFE[] | null;
+  scheduledTime?: Date;
+  excelName: string | null;
+  baseDir: string | null;
+  status: "대기중" | "예약완료" | "작업중" | "작업완료" | "작업실패" | "취소됨";
+  debug: boolean;
+};
+
+export type NaraG2bDataFE = {
+  query: string; // 검색어 - 신발장
+  startDate: string;
+  endDate: string;
+  organization?: string; // 기관명 - 개운중학교
+  location?: string; // 지역명 - 서울특별시교육청
+};
---
+++ frontend/src/features/naraG2b/utils/ipc.ts
@@ -0,0 +1,46 @@
+import type { NaraG2bDataFE, NaraG2bTaskFE } from "../types";
+
+export const getAllTasksIPC = (): Promise<NaraG2bTaskFE[]> => {
+  return window.ipcRenderer.invoke("naraG2b:getAllTasks");
+};
+
+export const addRowIPC = () => {
+  const id = Date.now().toString();
+  const newTask: NaraG2bTaskFE = {
+    id,
+    data: null,
+    excelName: null,
+    baseDir: null,
+    status: "대기중",
+    debug: false,
+  };
+  return window.ipcRenderer.invoke("naraG2b:addTask", newTask);
+};
+
+export const setScheduledTimeIPC = (id: string, scheduledTime: Date) => {
+  return window.ipcRenderer.invoke("naraG2b:updateTask", id, { scheduledTime });
+};
+
+export const setTaskQueryIPC = (id: string, excelName: string | null, data: NaraG2bDataFE[] | null) => {
+  return window.ipcRenderer.invoke("naraG2b:updateTask", id, { excelName, data });
+};
+
+export const initializeTaskIPC = (id: string, baseDir: string | null, debug: string | null) => {
+  return window.ipcRenderer.invoke("naraG2b:updateTask", id, { baseDir, debug });
+};
+
+export const updateTaskAllIPC = async (newValue: Partial<NaraG2bTaskFE>) => {
+  return window.ipcRenderer.invoke("naraG2b:updateTaskAll", newValue);
+};
+
+export const scheduledTaskIPC = (id: string) => {
+  return window.ipcRenderer.invoke("naraG2b:scheduleTask", id);
+};
+
+export const runTaskIPC = (id: string) => {
+  return window.ipcRenderer.invoke("naraG2b:runTask", id);
+};
+
+export const cancelTaskIPC = (id: string) => {
+  return window.ipcRenderer.invoke("naraG2b:cancelTask", id);
+};
