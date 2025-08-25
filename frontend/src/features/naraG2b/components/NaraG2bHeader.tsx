eda1e1dafe951711c0b505d7608932fa291f250f
NARARIA03 <hyeonseong0305@gmail.com>
2025-08-26 02:19:33 +0900

    refactor: 나라장터 설정 모달 import 시 네이밍 수정

--- frontend/src/features/naraG2b/components/NaraG2bHeader.tsx
+++ frontend/src/features/naraG2b/components/NaraG2bHeader.tsx
@@ -2,7 +2,7 @@ import { H2 } from "@/components";
 import { ArrowLeft, Settings } from "lucide-react";
 import { overlay } from "overlay-kit";
 import { useNavigate } from "react-router-dom";
-import SettingModal from "./NaraG2bSettingModal";
+import NaraG2bSettingModal from "./NaraG2bSettingModal";
 
 const NaraG2bHeader = () => {
   const navigate = useNavigate();
@@ -15,7 +15,7 @@ const NaraG2bHeader = () => {
       <H2>나라장터 페이지</H2>
       <Settings
         className="cursor-pointer hover:text-primary/60"
-        onClick={() => overlay.open(({ isOpen, close }) => <SettingModal isOpen={isOpen} onClose={close} />)}
+        onClick={() => overlay.open(({ isOpen, close }) => <NaraG2bSettingModal isOpen={isOpen} onClose={close} />)}
       />
     </div>
   );
