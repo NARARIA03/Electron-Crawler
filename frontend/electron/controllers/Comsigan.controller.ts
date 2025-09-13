708c115b01a9567bac6e6f9481a29dc949fa19d1
NARARIA03 <hyeonseong0305@gmail.com>
2025-09-13 22:07:01 +0900

    feat: controller/service 뼈대 작성

---
+++ frontend/electron/controllers/Comsigan.controller.ts
@@ -0,0 +1,149 @@
+import { BrowserWindow, shell } from "electron";
+import type { ComsiganCrawlData } from "electron/services/Comsigan.service";
+import ComsiganService from "electron/services/Comsigan.service";
+import path from "node:path";
+
+export type ComsiganTask = {
+  id: string;
+  data: ComsiganCrawlData[] | null;
+  excelName: string | null;
+  baseDir: string | null;
+  scheduledTime?: Date;
+  status: "대기중" | "예약완료" | "작업중" | "작업완료" | "작업실패" | "취소됨";
+  service?: ComsiganService;
+  debug: boolean;
+};
+
+class ComsiganController {
+  private static tasks = new Map<string, ComsiganTask>();
+  private static scheduledTasks = new Map<string, NodeJS.Timeout>();
+
+  /** READ */
+  public static getAllTasks() {
+    return [...this.tasks.values()].map(({ service: _, ...rest }) => rest);
+  }
+
+  /** CREATE */
+  public static addTask(task: ComsiganTask) {
+    this.tasks.set(task.id, task);
+    this.notifyUpdate();
+  }
+
+  /** UPDATE */
+  public static updateTask(id: string, newValue: Partial<ComsiganTask>) {
+    const task = this.tasks.get(id);
+    if (task) {
+      const newTask = { ...task, ...newValue };
+      this.tasks.set(id, newTask);
+      this.notifyUpdate();
+    }
+  }
+
+  public static updateTaskAll(newValue: Partial<ComsiganTask>) {
+    this.tasks.forEach((task, id) => {
+      const newTask = { ...task, ...newValue };
+      this.tasks.set(id, newTask);
+    });
+    this.notifyUpdate();
+  }
+
+  public static cancelTask(id: string) {
+    const task = this.tasks.get(id);
+    if (!task) return false;
+
+    // 예약 작업
+    const timer = this.scheduledTasks.get(id);
+    if (timer) {
+      clearTimeout(timer);
+      this.scheduledTasks.delete(id);
+      this.tasks.delete(id);
+      this.notifyUpdate();
+      return true;
+    }
+
+    // 실행 중 작업
+    if (task.service) {
+      task.service.close();
+      this.updateTask(id, { status: "취소됨", service: undefined });
+      return true;
+    }
+
+    // 나머지 작업
+    this.tasks.delete(id);
+    this.notifyUpdate();
+    return true;
+  }
+
+  public static async runTask(id: string) {
+    const task = this.tasks.get(id);
+    if (!task) throw new Error("id에 해당하는 태스크가 존재하지 않습니다");
+
+    const timer = this.scheduledTasks.get(id);
+    if (timer) {
+      clearTimeout(timer);
+      this.scheduledTasks.delete(id);
+    }
+
+    await this.executeTask(id);
+    return id;
+  }
+
+  public static scheduleTask(id: string) {
+    const task = this.tasks.get(id);
+    if (!task) throw new Error("id에 해당하는 태스크가 존재하지 않습니다");
+
+    const scheduledTime = task.scheduledTime;
+    if (!scheduledTime) throw new Error("태스크에 예약시간 값이 없습니다");
+
+    const delay = scheduledTime.getTime() - Date.now();
+
+    const timer = setTimeout(() => {
+      this.executeTask(id);
+      this.scheduledTasks.delete(id);
+    }, delay);
+
+    this.scheduledTasks.set(id, timer);
+    this.updateTask(id, { status: "예약완료" });
+
+    return id;
+  }
+
+  private static async executeTask(id: string) {
+    const task = this.tasks.get(id);
+    if (!task) return;
+    if (!task.baseDir) throw new Error("기본 저장 경로가 설정되지 않았습니다");
+    if (!task.excelName || !task.data) throw new Error("엑셀을 정상적으로 인식하지 못했습니다");
+
+    try {
+      this.updateTask(id, { status: "작업중" });
+      const service = new ComsiganService({
+        data: task.data,
+        excelName: task.excelName,
+        baseDir: task.baseDir,
+        debug: task.debug,
+      });
+      this.updateTask(id, { service });
+      console.log(this.tasks.get(id));
+
+      await service.crawl();
+      this.updateTask(id, { status: "작업완료", service: undefined });
+
+      const outputDir = path.join(task.baseDir, "excel_database", task.excelName.split(".")[0]);
+      shell.openPath(outputDir);
+    } catch (error) {
+      console.error(`Comsigan 크롤링 오류: ${error}`);
+      this.updateTask(id, { status: "작업실패", service: undefined });
+      const failDir = path.join(task.baseDir, "excel_database", task.excelName.split(".")[0]);
+      shell.openPath(failDir);
+    }
+  }
+
+  private static notifyUpdate() {
+    const allWindows = BrowserWindow.getAllWindows();
+    allWindows.forEach((window) => {
+      window.webContents.send("naraG2b:notifyUpdate", this.getAllTasks());
+    });
+  }
+}
+
+export default ComsiganController;
---
+++ frontend/electron/services/Comsigan.service.ts
@@ -0,0 +1,96 @@
+import puppeteer, { Browser } from "puppeteer";
+import LoggingService from "./Logging.service";
+import XlsxService from "./Xlsx.service";
+
+export type ComsiganCrawlData = {
+  query: string; // 검색어 - 신발장
+  startDate: string;
+  endDate: string;
+  organization?: string; // 기관명 - 개운중학교
+};
+
+type Params = {
+  data: ComsiganCrawlData[];
+  excelName: string;
+  baseDir: string;
+  debug: boolean;
+};
+
+class ComsiganService {
+  private data: Params["data"];
+  private excelName: Params["excelName"];
+  private baseDir: Params["baseDir"];
+  private debug: Params["debug"];
+
+  private browser: Browser | null = null;
+  private xlsxService: XlsxService | null = null;
+  private loggingService: LoggingService | null = null;
+
+  constructor({ data, excelName, baseDir, debug }: Params) {
+    this.data = data;
+    this.excelName = excelName;
+    this.baseDir = baseDir;
+    this.debug = debug;
+  }
+
+  private async setUp() {
+    this.browser = await puppeteer.launch({
+      headless: !this.debug,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--disable-gpu",
+      ],
+    });
+
+    this.loggingService = new LoggingService(this.baseDir, this.excelName);
+    this.xlsxService = new XlsxService(this.baseDir, this.excelName);
+    this.loggingService.logging(`엑셀 파일 생성 완료: ${this.xlsxService.getFilePath()}`);
+  }
+
+  public async close() {
+    if (this.browser) {
+      await this.browser.close();
+      this.browser = null;
+    }
+    if (this.loggingService) {
+      this.loggingService.save();
+      this.loggingService = null;
+    }
+    if (this.xlsxService) {
+      await this.xlsxService.save();
+      this.xlsxService = null;
+    }
+    await this.delay(5000);
+  }
+
+  private async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  public async crawl() {
+    for (const crawlData of this.data) {
+      try {
+        await this.setUp();
+        await this.query(crawlData);
+      } catch (error) {
+        this.loggingService?.errorLogging("크롤링 중 에러 발생", error);
+      } finally {
+        await this.close();
+      }
+    }
+  }
+
+  private async query({ query, startDate, endDate, organization }: ComsiganCrawlData) {
+    if (!this.browser) throw new Error("브라우저 초기화 실패");
+    if (!this.loggingService) throw new Error("에러 로깅 서비스 초기화 실패");
+    if (!this.xlsxService) throw new Error("xlsx 서비스 초기화 실패");
+    if (!organization) throw new Error("organization이 비어있습니다");
+  }
+}
+
+export default ComsiganService;
