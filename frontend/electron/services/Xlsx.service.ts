46858e43c9624697014f637aec7a31d88f66ccf6
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-15 23:55:14 +0900

    fix: 엑셀 Worksheet를 매 번 새로 생성해서 덮어쓰는 문제 해결

--- frontend/electron/services/Xlsx.service.ts
+++ frontend/electron/services/Xlsx.service.ts
@@ -16,12 +16,16 @@ type TErrorParams = TBaseParams & {
   message: string;
 };
 
-class XlsxService {
+export default class XlsxService {
   private wb: ExcelJS.Workbook;
-  private ws: ExcelJS.Worksheet;
+  private ws: ExcelJS.Worksheet | null;
   private filePath: string;
   private readonly defaultDirName = "excel_database";
+  private readonly sheetName = "Sheet1";
 
+  /**
+   * @description Workbook과 변수를 초기화하는 생성자
+   */
   constructor(baseDir: string, excelName: string) {
     const excelBaseName = excelName.split(".")[0];
     const excelDir = path.join(baseDir, this.defaultDirName, excelBaseName);
@@ -31,12 +35,40 @@ class XlsxService {
 
     this.filePath = path.join(excelDir, excelName);
     this.wb = new ExcelJS.Workbook();
-    this.ws = this.wb.addWorksheet("Sheet1", { pageSetup: { fitToPage: true } });
-    this.ws.addRow(["검색어", "기관명", "정보제목", "파일링크"]);
+    this.ws = null;
   }
 
+  /**
+   * @description 초기 Worksheet를 생성해서 반환하는 메서드
+   */
+  private createWorksheet() {
+    const ws = this.wb.addWorksheet(this.sheetName, { pageSetup: { fitToPage: true } });
+    ws.addRow(["검색어", "기관명", "정보제목", "파일링크"]);
+    return ws;
+  }
+
+  /**
+   * @description 싱글톤 형태로 Worksheet 반환을 보장하는 메서드
+   */
+  private async getWorksheet() {
+    if (!this.ws) {
+      if (fs.existsSync(this.filePath)) {
+        await this.wb.xlsx.readFile(this.filePath);
+        this.ws = this.wb.getWorksheet(this.sheetName) ?? this.createWorksheet();
+      } else {
+        this.ws = this.createWorksheet();
+      }
+    }
+    return this.ws;
+  }
+
+  /**
+   * @description 정상 데이터 수집 후, 행을 추가하는 메서드
+   * @note `Promise.all` 등으로 race condition을 유발하지 마세요.
+   */
   public async addRow({ query, organization, title, fileLink }: TSuccessParams) {
-    const row = this.ws.addRow([query, organization, title, "파일 열기"]);
+    const ws = await this.getWorksheet();
+    const row = ws.addRow([query, organization, title, "파일 열기"]);
 
     const linkCell = row.getCell(4);
     linkCell.value = { text: "파일 열기", hyperlink: fileLink };
@@ -45,8 +77,13 @@ class XlsxService {
     await this.save();
   }
 
+  /**
+   * @description 에러 발생 시 행을 추가하는 메서드
+   * @note `Promise.all` 등으로 race condition을 유발하지 마세요.
+   */
   public async addErrorRow({ query, organization, title, message }: TErrorParams) {
-    const row = this.ws.addRow([query, organization, title, message]);
+    const ws = await this.getWorksheet();
+    const row = ws.addRow([query, organization, title, message]);
 
     const errorCell = row.getCell(4);
     errorCell.font = { color: { argb: "FFFF0000" } };
@@ -54,10 +91,14 @@ class XlsxService {
     await this.save();
   }
 
-  private autoFitColumns() {
+  /**
+   * @description 저장 전 셀의 width를 맞추는 메서드
+   */
+  private async autoFitColumns() {
     const colWidths: number[] = [];
+    const ws = await this.getWorksheet();
 
-    this.ws.eachRow((row) => {
+    ws.eachRow((row) => {
       row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
         const cellValue = cell.value ? String(cell.value) : "";
         let cellWidth = 0;
@@ -69,17 +110,21 @@ class XlsxService {
       });
     });
 
-    this.ws.columns = colWidths.map((width) => ({ width }));
+    ws.columns = colWidths.map((width) => ({ width }));
   }
 
+  /**
+   * @description xlsx 파일에 데이터를 저장하는 메서드
+   */
   public async save() {
-    this.autoFitColumns();
+    await this.autoFitColumns();
     await this.wb.xlsx.writeFile(this.filePath);
   }
 
+  /**
+   * @description xlsx 파일 경로를 반환하는 메서드
+   */
   public getFilePath(): string {
     return this.filePath;
   }
 }
-
-export default XlsxService;
