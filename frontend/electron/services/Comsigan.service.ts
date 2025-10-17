3b9a97d9f1dbec890c88fd9a99c0ca7b4e04d10e
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-18 05:20:13 +0900

    feat: 학교를 찾아서 검색하는 로직 구현

--- frontend/electron/services/Comsigan.service.ts
+++ frontend/electron/services/Comsigan.service.ts
@@ -74,7 +74,6 @@ class ComsiganService {
           "--disable-gpu",
           "--disable-features=HttpsFirstBalancedModeAutoEnable", // HTTPS 안 되는 사이트 접근 시 뜨는 경고 무시하는 flag
         ],
-        slowMo: 250,
         executablePath: this.getExecutablePath(),
       });
 
@@ -125,17 +124,51 @@ class ComsiganService {
     if (!this.xlsxService) throw new Error("xlsx 서비스 초기화 실패");
 
     const page = await this.browser.newPage();
-
     await page.setViewport({ width: 1080, height: 1024 });
-    await page.goto("http://xn--s39aj90b0nb2xw6xh.kr/");
+    await page.goto("http://xn--s39aj90b0nb2xw6xh.kr/", {
+      waitUntil: "networkidle2",
+      timeout: 30000,
+    });
+    this.loggingService.logging("페이지 로딩 완료");
 
-    await page.reload();
+    const targetIFrame = page.frames().find((frame) => frame.url().includes("comci.net:4082/st"));
+    if (!targetIFrame) throw new Error("대상 frame을 찾을 수 없습니다");
+    this.loggingService.logging("iframe 내부 접근 성공");
 
-    await this.delay(3000);
+    await targetIFrame.locator("input#sc").fill(schoolName);
+    this.loggingService.logging("학교명 입력 완료");
+    await targetIFrame.locator('input[value="검색"]').click();
+    this.loggingService.logging("검색 버튼 클릭 완료");
 
-    await page.locator('input[value="학교찾기"]').click();
+    await page.waitForResponse((response) => response.url().includes("comci.net:4082") && response.status() === 200);
+    this.loggingService.logging("학교명 검색 API 200 확인");
 
-    await this.delay(3000);
+    this.loggingService.logging("검색 결과에서 학교를 찾아 클릭 시도");
+    const isSuccess = await targetIFrame.evaluate(
+      (targetSchoolName, targetRegion) => {
+        const table = document.querySelector("table#학교명단검색");
+        const searchResults = Array.from(table?.querySelectorAll("tbody tr") ?? []);
+
+        for (const row of searchResults) {
+          const cells = row.querySelectorAll("td");
+          if (cells.length < 2) continue;
+          const regionText = cells[0]?.innerText?.trim() ?? "";
+          const link = cells[1]?.querySelector("a");
+          const schoolText = link?.innerText?.trim() ?? "";
+
+          if (regionText.includes(targetRegion) && schoolText.includes(targetSchoolName)) {
+            link?.click();
+            return true;
+          }
+        }
+        return false;
+      },
+      schoolName,
+      region
+    );
+
+    if (!isSuccess) throw new Error(`${region}-${schoolName} 검색 결과가 없습니다.`);
+    this.loggingService.logging(`학교 선택 완료. 지역: ${region}, 학교명: ${schoolName}`);
 
     await this.delay(10000);
   }
