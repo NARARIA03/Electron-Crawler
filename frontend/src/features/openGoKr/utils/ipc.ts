7c8bb2b04a3ddadd114eb1156890127d4c845300
NARARIA03 <hyeonseong0305@gmail.com>
2025-10-31 16:58:22 +0900

    refactor: ipc key 상수화

--- frontend/src/features/openGoKr/utils/ipc.ts
+++ frontend/src/features/openGoKr/utils/ipc.ts
@@ -1,7 +1,9 @@
 import type { OpenGoKrTaskFE } from "../types";
 
+const PREFIX = "openGoKr";
+
 export const getAllTasksIPC = (): Promise<OpenGoKrTaskFE[]> => {
-  return window.ipcRenderer.invoke("openGoKr:getAllTasks");
+  return window.ipcRenderer.invoke(`${PREFIX}:getAllTasks`);
 };
 
 export const addRowIPC = () => {
@@ -14,33 +16,33 @@ export const addRowIPC = () => {
     status: "대기중",
     debug: null,
   };
-  return window.ipcRenderer.invoke("openGoKr:addTask", newTask);
+  return window.ipcRenderer.invoke(`${PREFIX}:addTask`, newTask);
 };
 
 export const setScheduledTimeIPC = (id: string, scheduledTime: Date) => {
-  return window.ipcRenderer.invoke("openGoKr:updateTask", id, { scheduledTime });
+  return window.ipcRenderer.invoke(`${PREFIX}:updateTask`, id, { scheduledTime });
 };
 
 export const setTaskQueryIPC = (id: string, excelName: string | null, data: unknown[] | null) => {
-  return window.ipcRenderer.invoke("openGoKr:updateTask", id, { excelName, data });
+  return window.ipcRenderer.invoke(`${PREFIX}:updateTask`, id, { excelName, data });
 };
 
 export const initializeTaskIPC = (id: string, baseDir: string | null, debug: string | null) => {
-  return window.ipcRenderer.invoke("openGoKr:updateTask", id, { baseDir, debug });
+  return window.ipcRenderer.invoke(`${PREFIX}:updateTask`, id, { baseDir, debug });
 };
 
 export const updateTaskAllIPC = (newValue: Partial<OpenGoKrTaskFE>) => {
-  return window.ipcRenderer.invoke("openGoKr:updateTaskAll", newValue);
+  return window.ipcRenderer.invoke(`${PREFIX}:updateTaskAll`, newValue);
 };
 
 export const scheduledTaskIPC = (id: string) => {
-  return window.ipcRenderer.invoke("openGoKr:scheduleTask", id);
+  return window.ipcRenderer.invoke(`${PREFIX}:scheduleTask`, id);
 };
 
 export const runTaskIPC = (id: string) => {
-  return window.ipcRenderer.invoke("openGoKr:runTask", id);
+  return window.ipcRenderer.invoke(`${PREFIX}:runTask`, id);
 };
 
 export const cancelTaskIPC = (id: string) => {
-  return window.ipcRenderer.invoke("openGoKr:cancelTask", id);
+  return window.ipcRenderer.invoke(`${PREFIX}:cancelTask`, id);
 };
